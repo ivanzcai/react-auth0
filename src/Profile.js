cc625c56c726f98dc1b389d62f671342eff19685
zcai2672 <ivan.z.cai@gmail.com>
2019-04-28 22:10:04 +1000

    complete chapter 3

---
+++ src/Profile.js
@@ -0,0 +1,15 @@
+import React, {
+    Component
+} from 'react';
+
+class Profile extends Component {
+    render() {
+        return ( 
+        <h1>
+            Profile 
+        </h1>
+        );
+    }
+}
+
+export default Profile;
