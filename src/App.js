38a2696f4a81b9d6a113e2894b23bbe58010a3fa
Ivan Cai <Ivan.Cai@asggroup.com.au>
2019-04-29 09:56:25 +1000

    added env and auth0 configuration

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React from 'react';
-import { Route } from "react-router-dom"
+import {
+  Route
+} from "react-router-dom"
 import Home from './Home'
 import Profile from './Profile'
 import Nav from './Nav'
 
 function App() {
-  return (
-    <>
-    <Nav />
-      <Route path="/" exact component={Home} />
-      <Route path="/profile" component={Profile} />
-    </>
+  return ( <
+    >
+    <
+    Nav / >
+    <
+    Route path = "/"
+    exact component = {
+      Home
+    }
+    /> <
+    Route path = "/profile"
+    component = {
+      Profile
+    }
+    /> <
+    />
   );
 }
 
